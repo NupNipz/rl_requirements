4cdbc00874f0a9bc7fc21c1457d1a0afcea0fe16
NupNipz <88530484+NupNipz@users.noreply.github.com>
2023-06-30 10:14:28 +0200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,6 +1,2 @@
 rlConfig = {}
-
--- Config option is only relevant when using qb-inventory!
-rlConfig.Framework = "qb-core" -- qb-core
-
 rlConfig.Inventory = "ox_inventory" -- qb-inventory or ox_inventory
