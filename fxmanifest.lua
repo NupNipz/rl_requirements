c3094770466107c93e6d1d4fdc35c9f09d12a5eb
NupNipz <88530484+NupNipz@users.noreply.github.com>
2023-06-30 14:01:09 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,10 @@ fx_version "cerulean"
 game "gta5"
 lua54 "yes"
 
+shared_scripts { -- Uncomment this if using ox_lib while using ESX
+    -- "@ox_lib/init.lua", 
+}
+
 client_scripts {
     "Config.lua",
     "Client/Client*.lua",
@@ -17,5 +21,6 @@ ui_page "html/Index.html"
 files {
     "html/Index.html",
     "html/Script.js",
-    "html/Style.css"
+    "html/Style.css",
+    "html/Images/*.png"
 }
