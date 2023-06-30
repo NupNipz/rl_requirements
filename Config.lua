3260538325303c45c99713630a7bef63d5be811f
NupNipz <88530484+NupNipz@users.noreply.github.com>
2023-06-30 19:29:58 +0200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,5 +1,6 @@
 rlConfig = {}
 rlConfig.Inventory = "ESX" -- qb-inventory, ox_inventory or ESX
 
--- If ur using ESX make sure you place item images in the (Image) folder, located in the (HTML) folder. otherwise boxes will be displayed without any image
+-- If ur using ESX stock F2 inventory, make sure you place item images in the (Image) folder, located in the (HTML) folder. otherwise boxes will be displayed without any image
+-- If ur using ESX stock F2 inventory, the rlConfig.Imagepath is not required
 rlConfig.ImagePath = "/web/images/" -- Image path, ur inventory name will be pasted in front by the script.
