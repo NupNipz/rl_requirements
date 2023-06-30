db4317379afad7194b3fdc2a0e8fbbfc4ec4aa0c
NupNipz <88530484+NupNipz@users.noreply.github.com>
2023-06-30 14:01:07 +0200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -3,4 +3,5 @@ rlConfig.Inventory = "ESX" -- qb-inventory, ox_inventory or ESX
 rlConfig.ImagePath = "/web/images/" -- Image path, ur inventory name will be pasted in front by the script.
 
 -- ESX related
+-- If ur using ESX make sure you place item images in the (Image) folder, located in the (HTML) folder. otherwise boxes will be displayed without any image
 rlConfig.UseOxLib = true -- If true it will use ox_lib callback, else it will use ESX callbacks
