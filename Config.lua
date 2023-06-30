96b37d2375295e3f5db1324ce9b88dc932190997
NupNipz <88530484+NupNipz@users.noreply.github.com>
2023-06-30 10:48:29 +0200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,2 +1,3 @@
 rlConfig = {}
 rlConfig.Inventory = "ox_inventory" -- qb-inventory or ox_inventory
+rlConfig.ImagePath = "/web/images/" -- Image path, ur inventory name will be pasted in front by the script.
